503c716ecaa87daa1e14fae51ea5e2ec96b8ce1d
aisav <artur.isaverdyan@gmail.com>
2018-12-14 00:19:30 +0400

    fixes

--- src/store/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const setVisibilityFilter = 'SET_VISIBILITY_FILTER';
-export const toggleTodo = 'TOGGLE_TODO';
